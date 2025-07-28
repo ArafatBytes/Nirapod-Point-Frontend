422d39d5bf5a6deba51f646818e064641e19a137
ArafatBytes <mdarafat1661@gmail.com>
2025-07-28 18:14:37 +0600

    Added production backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api": "https://nirapod-point-backend.onrender.com",
     },
   },
 });
